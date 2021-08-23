a763c7ff5e3d9e2724704615a4b3525bd481d19f
Zain-mir24 <zainnaeemk10@gmail.com>
2021-08-23 04:57:20 -0700

    2boxes

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Homepage.css"
+function Homepage() {
+    return (
+        <div className="container">
+            <div className="left">
+                1stbox
+            </div>
+            <div className="right">
+               2ndbox
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
