fd1374086bb7735d0ef19752681a8c66d194ac19
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2021-08-23 20:10:16 +0500

    Started Creating whymyplegebox

---
+++ src/components/whymypflegebox/whymypflegebox.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./styles.css"
+function Homepage() {
+    return (
+        <div className="container">
+            <div className="left">
+            </div>
+            <div className="right">
+               <div className="centerDiv">
+                   <div  className="para">
+                   <p>
+                       The way to your free care box
+                   </p>
+                   </div>
+                
+               </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
