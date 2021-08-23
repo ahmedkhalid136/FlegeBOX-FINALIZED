b1b4c58c2d4263f7ec123de7dcf227df5e8f9f0f
Zain-mir24 <zainnaeemk10@gmail.com>
2021-08-23 06:37:08 -0700

    left para done

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -4,13 +4,15 @@ function Homepage() {
     return (
         <div className="container">
             <div className="left">
-               
             </div>
             <div className="right">
                <div className="centerDiv">
+                   <div  className="para">
                    <p>
                        The way to your free care box
                    </p>
+                   </div>
+                
                </div>
             </div>
             <div>
