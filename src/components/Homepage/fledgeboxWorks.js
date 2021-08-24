5500b930216c0d0310ba50ebeaaf3a40122c623b
Zain-mir24 <zainnaeemk10@gmail.com>
2021-08-24 03:18:27 -0700

    chalega

--- src/components/Homepage/fledgeboxWorks.js
+++ src/components/Homepage/fledgeboxWorks.js
@@ -1,14 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import "./Homepage.css";
 function FledgeboxWorks() {
-    return (
+  return (
+    <div>
+      <h1>How your MyPflegebox Works</h1>
+      <div className="d-flex justify-content-center">
         <div>
-            <h1>
-               How your MyPflegebox Works
-            </h1>
-          
+          <p className="text">1.Wish core box select</p>
+        
         </div>
-    )
+        <div>
+          <p className="text">2.Request nonline fillout and send</p>
+        </div>
+        <div>
+          <p className="text">
+            3.We take over application & billing with nursing care insurance
+          </p>
+        </div>
+        <div >
+            <p className="text">
+            4.recieve monthly your free care box
+            </p>
+          </div>
+      </div>
+    </div>
+  );
 }
 
-export default FledgeboxWorks
+export default FledgeboxWorks;
