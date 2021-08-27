5e8d6bcc3332614869d06b10343c727866a683ad
Zain-mir24 <zainnaeemk10@gmail.com>
2021-08-27 09:15:44 -0700

    Done FAQ

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -3,10 +3,24 @@ import "./FAQ.css";
 
 function Faq() {
   const [Color, setColor] = useState(true);
+  const [Color2,setColor2] = useState(true)
+  const [Color3,setColor3]=useState(true)
+  const [Color4,setColor4] =useState(true)
+  const [Color5,setColor5]=useState(true)
+  const [Color6,setColor6]=useState(true)
+  const [Color7,setColor7]=useState(true)
+  
+
+
 
   const [getIcon, setIcon] = useState(true);
   const [getIcon2, setIcon2] = useState(true);
   const [getIcon3, setIcon3] = useState(true);
+  const [getIcon4, setIcon4] = useState(true);
+  const [getIcon5, setIcon5] = useState(true);
+  const [getIcon6, setIcon6] = useState(true);
+  const [getIcon7, setIcon7] = useState(true);
+  
   
   return (
     <div className="mainDiv">
@@ -36,15 +50,17 @@ function Faq() {
           </div>
         </div>
       </p>
+      {/* Second div */}
 
       <p className="questionDiv">
         <h6
-          className="text1"
+           className={Color2 === true ?"text1":"activeText1"}
           onClick={() => {
             setIcon2(!getIcon2);
-         
+            setColor2(!Color2)
+           
           }}
-          style={{ color: Color }}
+          style={{ color: Color2 }}
           data-bs-toggle="collapse"
           href="#collapseExample1"
           role="button"
@@ -62,11 +78,14 @@ function Faq() {
           </div>
         </div>
       </p>
-
+{/* THIRD DIVV */}
       <p className="questionDiv">
         <h6
-          className="text1"
-          onClick={() => setIcon3(!getIcon3)}
+          className={Color3 === true ?"text1":"activeText1"}
+          onClick={() => {setIcon3(!getIcon3)
+            setColor3(!Color3)
+          }}
+          style={{ color: Color3 }}
           data-bs-toggle="collapse"
           href="#collapseExample2"
           role="button"
@@ -77,6 +96,107 @@ function Faq() {
           Link with href
         </h6>
         <div class="collapse" id="collapseExample2">
+          <div class="answerDiv">
+            Some placeholder content for the collapse component. This panel is
+            hidden by default but revealed when the user activates the relevant
+            trigger.
+          </div>
+        </div>
+      </p>
+      {/* Fourth div */}
+      <p className="questionDiv">
+        <h6
+          className={Color4 === true ?"text1":"activeText1"}
+          onClick={() => {setIcon4(!getIcon4)
+            setColor4(!Color4)
+          }}
+          style={{ color: Color4 }}
+          data-bs-toggle="collapse"
+          href="#collapseExample3"
+          role="button"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+          <i class={getIcon4=== true ? "fas fa-plus" : "fas fa-minus"}></i>{" "}
+          Link with href
+        </h6>
+        <div class="collapse" id="collapseExample3">
+          <div class="answerDiv">
+            Some placeholder content for the collapse component. This panel is
+            hidden by default but revealed when the user activates the relevant
+            trigger.
+          </div>
+        </div>
+      </p>
+      {/* Fifth div */}
+      <p className="questionDiv">
+        <h6
+          className={Color5 === true ?"text1":"activeText1"}
+          onClick={() => {setIcon5(!getIcon5)
+            setColor5(!Color5)
+          }}
+          style={{ color: Color5 }}
+          data-bs-toggle="collapse"
+          href="#collapseExample4"
+          role="button"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+          <i class={getIcon5 === true ? "fas fa-plus" : "fas fa-minus"}></i>{" "}
+          Link with href
+        </h6>
+        <div class="collapse" id="collapseExample4">
+          <div class="answerDiv">
+            Some placeholder content for the collapse component. This panel is
+            hidden by default but revealed when the user activates the relevant
+            trigger.
+          </div>
+        </div>
+      </p>
+
+      {/* sixth div */}
+      <p className="questionDiv">
+        <h6
+          className={Color6 === true ?"text1":"activeText1"}
+          onClick={() => {setIcon6(!getIcon6)
+            setColor6(!Color6)
+          }}
+          style={{ color: Color6 }}
+          data-bs-toggle="collapse"
+          href="#collapseExample5"
+          role="button"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+          <i class={getIcon6 === true ? "fas fa-plus" : "fas fa-minus"}></i>{" "}
+          Link with href
+        </h6>
+        <div class="collapse" id="collapseExample5">
+          <div class="answerDiv">
+            Some placeholder content for the collapse component. This panel is
+            hidden by default but revealed when the user activates the relevant
+            trigger.
+          </div>
+        </div>
+      </p>
+
+      <p className="questionDiv">
+        <h6
+          className={Color7 === true ?"text1":"activeText1"}
+          onClick={() => {setIcon7(!getIcon7)
+            setColor7(!Color7)
+          }}
+          style={{ color: Color7 }}
+          data-bs-toggle="collapse"
+          href="#collapseExample6"
+          role="button"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
+          <i class={getIcon6 === true ? "fas fa-plus" : "fas fa-minus"}></i>{" "}
+          Link with href
+        </h6>
+        <div class="collapse" id="collapseExample6">
           <div class="answerDiv">
             Some placeholder content for the collapse component. This panel is
             hidden by default but revealed when the user activates the relevant
