5595267b6e923a1dc82d31fe71511c378b6c374b
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2021-08-28 22:59:06 +0500

    Created Benifits Section Homepage

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -3,12 +3,14 @@ import "./Homepage.css";
 import Button from "../button/Button";
 import Title from "./Title.js";
 import Works from "./Works.js";
+import Benifits from "./Benifits";
 
 function Homepage() {
   return (
     <div>
       <Title />
       <Works />
+      <Benifits />
     </div>
   );
 }
--- src/components/Homepage/Works.js
+++ src/components/Homepage/Works.js
@@ -2,13 +2,32 @@ import React from "react";
 
 export default function Works() {
   return (
-    <div className="container">
+    <div className="container Works">
       <h2>How Your MyPflegebox Works</h2>
       <div className="row">
-        <div className="col-lg-3"></div>
-        <div className="col-lg-3"></div>
-        <div className="col-lg-3"></div>
-        <div className="col-lg-3"></div>
+        <div className="col-lg-3">
+          <h3>
+            1. Wish core box <br /> select
+          </h3>
+        </div>
+        <div className="col-lg-3">
+          <h3>
+            2. Request online fill out <br /> and send{" "}
+          </h3>
+        </div>
+        <div className="col-lg-3">
+          <h3>
+            3. We take over <br />
+            Application <br /> & billing with the <br />
+            nursing care insurance
+          </h3>
+        </div>
+        <div className="col-lg-3">
+          <h3>
+            4. Receive monthly your <br />
+            free care box
+          </h3>
+        </div>
       </div>
     </div>
   );
