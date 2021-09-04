06a4b1ab5e67aaa891a97fd6dcbdb5a2e69ba884
Zain-mir24 <zainnaeemk10@gmail.com>
2021-09-04 05:57:35 -0700

    background buttons added

--- src/components/Box/Whiteproducts.js
+++ src/components/Box/Whiteproducts.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React,{useState} from "react";
 import Image1 from "../../Pictures/clay-banks-e6pK_snssSY-unsplash.jpg";
 import "./Whitebox.css";
 
 export default function WhiteProduct() {
+  const [getBackground,setBackground]=useState(true)
+  const [getBackground2,setBackground2]=useState(true)
+  const [getBackground3,setBackground3]=useState(true)
   return (
     <div
       className="row"
@@ -26,9 +29,15 @@ export default function WhiteProduct() {
       <div className="col-lg-6" style={{ paddingTop: "20px" }}>
         <div className="row">
           <div className="col-lg-12">
-            <button className="increment-buttons1 sizes1">S</button>
-            <button className="increment-buttons1 sizes1">M</button>
-            <button className="increment-buttons1 sizes1">L</button>
+            <button onClick={()=>{
+              setBackground(!getBackground)
+            }} className={getBackground?"sizes1":"increment-buttons1"}>S</button>
+            <button onClick={()=>{
+              setBackground2(!getBackground2)
+            }} className={getBackground2?"sizes1":"increment-buttons1"}>M</button>
+            <button onClick={()=>{
+              setBackground3(!getBackground3)
+            }} className={getBackground3?"sizes1":"increment-buttons1"}>L</button>
           </div>
       
         </div>
