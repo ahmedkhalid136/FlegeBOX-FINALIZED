2169822a5ea2d6a80f1701145538f9a62467b82c
Zain-mir24 <zainnaeemk10@gmail.com>
2021-09-04 01:09:01 -0700

    Fillinfo page final

--- src/components/FillInformation/FillInfo.js
+++ src/components/FillInformation/FillInfo.js
@@ -5,25 +5,79 @@ import Contactinfo from "./Contactinfo";
 import Carelevel from "./Carelevel";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Note from "./Note";
-import Footer from "../footer/Footer";
+import Footer from "../footer/Footer"
+
 
 function FillInfo() {
   return (
-    <div className="mainDiv">
+  
+        <div className="mainDiv">
+      
       <div className="personalInfo">
+      
         <div className="checkboxes">
-          <h6 className="styleHeading">Personal Information</h6>
+        <h6 className="styleHeading">Personal Information</h6>
 
-          <div className="personalInfo-section">
-            <h6 className="styleHeading">Personal Information</h6>
+          <FormControlLabel
+            value="Ms"
+            control={<Checkbox color="primary" />}
+            label="Ms"
+            labelPlacement="End"
+          />
+          <FormControlLabel
+            value="Mr"
+            control={<Checkbox color="primary" />}
+            label="Mr"
+            labelPlacement="End"
+          />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 col-sm-12 ">
+              <input className="input" placeholder="Firstname" />
+            </div>
+            <div className="col-lg-6 col-sm-12">
+              <input className="input" placeholder="Lastname" />
+            </div>
           </div>
-
-          <Carelevel />
-          <Contactinfo />
-          <Note />
+          <div className="row secondDiv">
+            <div className="col-lg-6">
+              <div className="row">
+                <div className="col-lg-6 col-sm-8 col-xs-8">
+                  <input className="input1" placeholder="Streetname" />
+                </div>
+                <div className="col-lg63 col-sm-4 col-xs-4">
+                  <input className="input2" placeholder="N*" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="row">
+                <div className="col-lg-6 col-sm-8  col-xs-8">
+                  <input className="input3" placeholder="ZIP Code" />
+                </div>
+                <div className="col-lg-6 col-sm-4 col-xs-4">
+                  <input className="input4" placeholder="Country" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="checkboxes">
+          <FormControlLabel
+            value="Ms"
+            control={<Checkbox color="primary" />}
+            label="Different address"
+            labelPlacement="End"
+          />
         </div>
       </div>
-      <Footer />
+
+      <Carelevel /> 
+      <Contactinfo />
+      <Note />
+      
     </div>
   );
 }
