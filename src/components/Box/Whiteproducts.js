56641e277e5c1cf7ffdbf6c9c7f6d0505c78a537
Zain-mir24 <zainnaeemk10@gmail.com>
2021-09-04 03:52:13 -0700

    chooseBox 2 options

---
+++ src/components/Box/Whiteproducts.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Box.css";
+function Whiteproducts() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-lg-6">
+          <div className="row">
+            <div className="col-lg-2">
+              <div>image</div>
+            </div>
+            <div className="col-lg-4 ">
+                <div >
+                      N95 mask
+                                 
+                </div>
+              
+                </div>
+          </div>
+        </div>
+        <div className="col-lg-6">S M L</div>
+      </div>
+    </div>
+  );
+}
+
+export default Whiteproducts;
