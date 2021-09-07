1025928b59f00e557dd7bec91bc3104e0a8a23a8
Zain-mir24 <zainnaeemk10@gmail.com>
2021-09-07 01:44:18 -0700

    header is placed on 3 pages

--- src/components/Confirmation/Confirmation.js
+++ src/components/Confirmation/Confirmation.js
@@ -3,10 +3,27 @@ import Body from "./Body";
 import "./styles.css";
 import "../footer/Footer";
 import Footer from "../footer/Footer";
-
+import Header from "../Header/Header"
 export default function Confirmation() {
   return (
     <div>
+      <Header />
+        <div className="container head">
+          <div className="row">
+            <div className="col-lg-3">
+              1. CHOOSE BOX
+            </div>
+            <div className="col-lg-2">...........................</div>
+            <div className="col-lg-3">
+              2.FILLINFORMATION
+            </div>
+            <div className="col-lg-2">...........................</div>
+            <div className="col-lg-2">
+              <button className="chooseButton">3. CONFIRMATION</button>
+            </div>
+            <div className="col-lg-1"></div>
+          </div>
+        </div>
       <Body />
       <Footer />
     </div>
