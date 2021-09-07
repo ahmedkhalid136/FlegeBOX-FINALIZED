eb393dbebc095095537d73b68b91fad1e70c100e
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2021-09-08 00:46:48 +0500

    Testing

--- src/components/footer/Base.js
+++ src/components/footer/Base.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+const ColoredLine = ({ color }) => (
+  <hr
+    style={{
+      color: color,
+      backgroundColor: color,
+      height: 5,
+    }}
+  />
+);
+
 export default function Base() {
   return (
     <div className="footer-base">
@@ -7,7 +17,7 @@ export default function Base() {
         <div className="row">
           <div className="col-lg-4">
             <h5>MyPflegebox</h5>
-            <hr />
+            <ColoredLine color={"#f87433"} />
             <a>Why MyPflegebox ?</a>
             <br /> <br />
             <a>Blog</a> <br /> <br />
@@ -16,15 +26,15 @@ export default function Base() {
           </div>
           <div className="col-lg-4">
             <h5>Shipping Partner</h5>
-            <hr />
+            <hr className="line" />
           </div>
           <div className="col-lg-4">
             <h5>Payment Methods</h5>
-            <hr />
+            <hr className="line" />
           </div>
         </div>
 
-        <hr style={{ width: "100%", color: "white" }} />
+        <hr className="line" style={{ width: "100%", color: "white" }} />
         <div style={{ textAlign: "center" }}>
           <h2 className="copyright-text">
             © 2021 MyPflegebox. All right reserved
