33b684269f35456d063ec76b872d4bdff943e1db
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2021-09-09 12:23:34 +0500

    testing

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Box.css";
 import Header from "../Header/Header";
 import Footer from "../footer/Footer";
@@ -9,6 +9,47 @@ function Box() {
   const [getComp, setComp] = useState(true);
   const [getBox, setBox] = useState(true);
   const [getBox1, setBox1] = useState(true);
+
+  useEffect(() => {
+    console.log("test");
+    fetch("http://35.84.238.24/api/method/flegeapp.utils.create_patient"),
+      {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          Authorization: "token 6141d2161d30a42:b783e62c3c1518d",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: "John",
+          last_name: "Smith",
+          title: "MR",
+          street_name: "1092-Bayern",
+          date_of_birth: "2021-08-29",
+          zip_code: "940012",
+          country: "Germany",
+          care_level: "Level 1",
+          care_box: [
+            {
+              item: "gloves",
+              quantity: 2,
+              size: "Medium",
+            },
+            {
+              item: "hand-sanitizer",
+              quantity: 3,
+              size: "Large",
+            },
+          ],
+          phone_number: "+4919283292",
+          email_address: "flegepatient@patient.com",
+          insurance_company: "PAEO",
+          insurance_number: "PAEO-012930",
+          year: "2021",
+          note: "Patient from germany",
+        }),
+      };
+  });
 
   return (
     <div>
