3c689caeb9688fb218c59f6fef6b34b812762e4c
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2021-09-10 17:25:34 +0500

    Changes in Cart Page and box page

--- src/components/ProductComponent/Cart1.js
+++ src/components/ProductComponent/Cart1.js
@@ -1,10 +1,10 @@
-import React,{useState}from "react";
+import React, { useState } from "react";
 import "./BlueProduct.css";
 
 export default function Cart(props) {
-  const [getBackground,setBackground]=useState(true)
-  const [getBackground2,setBackground2]=useState(true)
-  const [getBackground3,setBackground3]=useState(true)
+  const [getBackground, setBackground] = useState(true);
+  const [getBackground2, setBackground2] = useState(true);
+  const [getBackground3, setBackground3] = useState(true);
   return (
     <div
       className="row cart-section"
@@ -16,32 +16,44 @@ export default function Cart(props) {
       }}
     >
       <div className="col-lg-2 col-3">
-        <img
-          className="product-image"
-          src={props.image}
-        />
+        <img className="product-image" src={props.image} />
       </div>
       <div
-        className="col-lg-4 col-2 product-name"
+        className="col-lg-4 col-4 product-name"
         style={{ textAlign: "left" }}
       >
-      <h6>{props.Name}</h6>
-      <p>{props.pcs} pcs</p>
+        <h6>{props.Name}</h6>
+        <p>{props.pcs}pcs</p>
       </div>
-      <div className="col-lg-6 col-7" style={{ paddingTop: "20px" }}>
-        <div className="increment-cart">
-        <button onClick={()=>{
-              setBackground(!getBackground)
-            }} className={getBackground?"sizes1":"increment-buttons1"}>S</button>
-            <button onClick={()=>{
-              setBackground2(!getBackground2)
-            }} className={getBackground2?"sizes1":"increment-buttons1"}>M</button>
-            <button onClick={()=>{
-              setBackground3(!getBackground3)
-            }} className={getBackground3?"sizes1":"increment-buttons1"}>L</button>
-          <div style={{ textAlign: "right", display: "inline-block" }}>
-            <i class="fas fa-times"></i>
-          </div>
+      <div className="col-lg-6 col-5" style={{ paddingTop: "20px" }}>
+        <div className="increment-cart cart1-sizes">
+          <button
+            onClick={() => {
+              setBackground(!getBackground);
+            }}
+            className={getBackground ? "sizes1" : "increment-buttons1"}
+          >
+            S
+          </button>
+          <button
+            onClick={() => {
+              setBackground2(!getBackground2);
+            }}
+            className={getBackground2 ? "sizes1" : "increment-buttons1"}
+          >
+            M
+          </button>
+          <button
+            onClick={() => {
+              setBackground3(!getBackground3);
+            }}
+            className={getBackground3 ? "sizes1" : "increment-buttons1"}
+          >
+            L
+          </button>
+        </div>
+        <div style={{ textAlign: "right", display: "inline-block" }}>
+          <i class="fas fa-times"></i>
         </div>
       </div>
     </div>
