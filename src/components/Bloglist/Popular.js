f9d2e31c4ce8bdd883f7c038732f6f6497b610c3
Zain-mir24 <zainnaeemk10@gmail.com>
2021-09-18 04:29:33 -0700

    my changes

--- src/components/Bloglist/Popular.js
+++ src/components/Bloglist/Popular.js
@@ -1,39 +1,34 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import Image2 from "../../Pictures/girl-with-red-hat-Epw-SqZYeyw-unsplash.jpg";
-import Blogdetails from '../Blogdetails/Blogdetails';
-import { Link} from 'react-router-dom';
+import Blogdetails from "../Blogdetails/Blogdetails";
+import { Link } from "react-router-dom";
 function Popular(props) {
-  
-  
-    return (
-      
-      
-                <div className="col-lg-4" style={{ textAlign: "left" }}>
-              <Link to={{
-                pathname:'/Blogdetails',
-                aboutProps:{
-                  title:props.title,
-                  content:props.content,
-                  image:props.image
-                }
-              }}>
-                <img src={props.image} className="bloglist-image" />
-                <br />
-                <br />
-                <h4>{props.title}</h4>
-                <br />
-                <p>
-                  {props.content}
-                </p>
-                <br />
-                <p>
-                  <i class="far fa-clock"></i> 4 MIN |{" "}
-                  <i class="far fa-calendar-alt"></i> JULY 20, 2021
-                </p>
-              </Link>
-            </div> 
-        
-    )
+  return (
+    <div className="col-lg-4" style={{ textAlign: "left" }}>
+      <Link
+        to={{
+          pathname: "/Blogdetails",
+          aboutProps: {
+            title: props.title,
+            content: props.content,
+            image: props.image,
+          },
+        }}
+      >
+        <img src={props.image} className="bloglist-image" />
+        <br />
+        <br />
+        <h4>{props.title}</h4>
+        <br />
+        <p>{props.content}</p>
+        <br />
+        <p>
+          <i class="far fa-clock"></i> 4 MIN |{" "}
+          <i class="far fa-calendar-alt"></i> JULY 20, 2021
+        </p>
+      </Link>
+    </div>
+  );
 }
 
-export default Popular
+export default Popular;
