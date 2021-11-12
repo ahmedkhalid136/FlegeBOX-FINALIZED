378ab8efd10c003ec32d4b6f21afbb5cf683dce9
AHMED KHALID <fa18-bcs-007@isbstudent.comsats.edu.pk>
2021-11-12 23:43:33 +0500

    H

--- src/components/ProductComponent/Cart1.js
+++ src/components/ProductComponent/Cart1.js
@@ -1,14 +1,15 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./BlueProduct.css";
 
 export default function Cart(props) {
   const [getBackground, setBackground] = useState(true);
   const [getBackground2, setBackground2] = useState(true);
   const [getBackground3, setBackground3] = useState(true);
-  const{Name,array,size}=props
-  const theObj = array.findIndex((obj) => obj.name == Name);
+  const { Name, array, size } = props;
+
+  const theObj = array.findIndex((obj) => obj.Name == Name);
+
   useEffect(() => {
-   
     if (size == "Small") {
       setBackground(!getBackground);
     } else if (size == "Medium") {
@@ -36,48 +37,48 @@ export default function Cart(props) {
       >
         <h6>{props.Name}</h6>
         <p>{props.pcs}pcs</p>
-       
       </div>
       <div className="col-lg-6 col-5" style={{ paddingTop: "20px" }}>
-       
-       {size==null?null: <div className="increment-cart cart1-sizes">
-          <button
-            onClick={() => {
-              setBackground(!getBackground);
-              setBackground3(true)
-              setBackground2(true)
-              array[theObj].size="Small"
-            }}
-            className={getBackground ? "sizes1" : "increment-buttons1"}
-          >
-            S
-          </button>
-          <button
-            onClick={() => {
-              setBackground2(!getBackground2);
-              setBackground(true)
-              setBackground3(true)
-              array[theObj].size="Medium"
-            }}
-            className={getBackground2 ? "sizes1" : "increment-buttons1"}
-          >
-            M
-          </button>
-          <button
-            onClick={() => {
-              setBackground3(!getBackground3);
-              setBackground(true)
-              setBackground2(true)
+        {size == null ? null : (
+          <div className="increment-cart cart1-sizes">
+            <button
+              onClick={() => {
+                setBackground(!getBackground);
+                setBackground3(true);
+                setBackground2(true);
+                array[theObj].size = "Small";
+              }}
+              className={getBackground ? "sizes1" : "increment-buttons1"}
+            >
+              S
+            </button>
+            <button
+              onClick={() => {
+                setBackground2(!getBackground2);
+                setBackground(true);
+                setBackground3(true);
+                array[theObj].size = "Medium";
+              }}
+              className={getBackground2 ? "sizes1" : "increment-buttons1"}
+            >
+              M
+            </button>
+            <button
+              onClick={() => {
+                setBackground3(!getBackground3);
+                setBackground(true);
+                setBackground2(true);
 
-              array[theObj].size="Large"
-             console.log(   "size is empty")
-            }}
-            className={getBackground3 ? "sizes1" : "increment-buttons1"}
-          >
-            L
-          </button>
-        </div>
-       } <div style={{ textAlign: "right", display: "inline-block" }}>
+                array[theObj].size = "Large";
+                console.log("size is empty");
+              }}
+              className={getBackground3 ? "sizes1" : "increment-buttons1"}
+            >
+              L
+            </button>
+          </div>
+        )}{" "}
+        <div style={{ textAlign: "right", display: "inline-block" }}>
           <i class="fas fa-times"></i>
         </div>
       </div>
